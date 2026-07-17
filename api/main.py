beb6eb21e04d3b3f9e28919548e50cf5e68f0fbf
Ananya Ghosh <ananyaghosh643@gmail.com>
2026-07-17 06:36:54 +0530

    promethus

--- api/main.py
+++ api/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 from src.pipeline.prediction_pipeline import PredictionPipeline
-
+from prometheus_client import Counter, Histogram, generate_latest
+from fastapi import Response
+import time
 
 app = FastAPI(
     title="Customer Churn Prediction API"
 )
 
+REQUEST_COUNT = Counter(
+    "prediction_requests_total",
+    "Total prediction requests"
+)
+
+REQUEST_LATENCY = Histogram(
+    "prediction_latency_seconds",
+    "Prediction latency"
+)
 
 class CustomerData(BaseModel):
 
@@ -42,12 +53,27 @@ def home():
         "message": "Customer Churn Prediction API"
     }
 
+@app.get("/metrics")
+def metrics():
+
+    return Response(
+        generate_latest(),
+        media_type="text/plain"
+    )
 
 @app.post("/predict")
 def predict(data: CustomerData):
 
+    REQUEST_COUNT.inc()
+
+    start = time.time()
+
     prediction = pipeline.predict(data.model_dump())
 
+    REQUEST_LATENCY.observe(
+        time.time() - start
+    )
+
     return {
-        "prediction": prediction.item()
+        "prediction": prediction
     }
